98b7458d411e00af7b3c5eb7a54b150645d38b21
Patrik Benyak <benyak@sited.sk>
2020-12-06 22:19:04 +0100

    Add forecast

---
+++ src/components/Forecast.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import {IonIcon, IonItem, IonLabel, IonList, IonListHeader, IonText, IonCard, IonCardContent, IonCardHeader, IonCardTitle} from '@ionic/react';
+import {cloudy, rainy, snow, sunny, thunderstorm, navigate} from 'ionicons/icons'
+import './Forecast.css';
+
+
+
+const icons: any = {
+  cloudy: cloudy,
+  partlycloudy_day: cloudy,
+  rain: rainy,
+  snow: snow,
+  clearsky_day: sunny,
+  heavyrainandthunder: thunderstorm,
+  lightsleet: rainy,
+  lightrain: rainy,
+}
+
+const formatDate = (date: Date|string) => {
+  const options = { weekday: 'short', year: 'numeric', month: 'long', day: 'numeric' };
+  const dateTimeFormat = new Intl.DateTimeFormat('sk-SK', options);
+  return dateTimeFormat.format(new Date(date));
+}
+
+interface ForecastProps {
+  title?: string;
+  forecast: any;
+}
+
+const Forecast: React.FC<ForecastProps> = ({title, forecast}) => {
+  const air_temperature = forecast.data['instant']?.details?.air_temperature
+  const symbol_code = forecast.data['next_12_hours']?.summary?.symbol_code
+  const precipitation_amount = forecast.data['next_6_hours']?.details?.precipitation_amount
+  const wind_from_direction = forecast.data['instant']?.details?.wind_from_direction
+  const wind_speed = forecast.data['instant']?.details?.wind_speed
+
+  return (
+    <IonCard>
+      <IonCardHeader>
+        <IonCardTitle>{title || formatDate(forecast.time)}</IonCardTitle>
+      </IonCardHeader>
+      <IonCardContent>
+    <IonList inset lines="none">
+      <IonItem>
+        <IonLabel>Obloha: </IonLabel>
+        <IonText color="secondary">
+          {icons[symbol_code]
+            ? <IonIcon color="primary" src={icons[symbol_code]} />
+            : symbol_code}
+        </IonText>
+      </IonItem>
+      <IonItem>
+        <IonLabel>Teplota: </IonLabel>
+        <IonText >
+          {air_temperature}°C
+        </IonText>
+      </IonItem>
+      <IonItem>
+        <IonLabel>Zrážky: </IonLabel>
+        <IonText >
+          {precipitation_amount}mm
+        </IonText>
+      </IonItem>
+      <IonItem>
+        <IonLabel>Vietor: </IonLabel>
+        <IonText >
+          {wind_speed} m/s
+          <IonIcon className="wind" color="primary" style={{'--wind-dir': `${wind_from_direction-45}deg`}} src={navigate} />
+        </IonText>
+      </IonItem>
+    </IonList>
+      </IonCardContent>
+    </IonCard>
+  )
+}
+
+export default Forecast
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,57 +1,94 @@
-import MessageListItem from '../components/MessageListItem';
+import Forecast from '../components/Forecast';
 import React, { useState } from 'react';
-import { Message, getMessages } from '../data/messages';
+import {cloudy, locate} from 'ionicons/icons';
 import {
   IonContent,
   IonHeader,
-  IonList,
+  IonFab,
+  IonFabButton,
   IonPage,
   IonRefresher,
   IonRefresherContent,
   IonTitle,
   IonToolbar,
-  useIonViewWillEnter
+  IonIcon,
+  IonSlides,
+  IonSlide,
 } from '@ionic/react';
 import './Home.css';
 
+
+
+
 const Home: React.FC = () => {
 
-  const [messages, setMessages] = useState<Message[]>([]);
+  const [data, setData] = useState<any>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [coords, setCoords] = useState<{lat: number, lon: number}>({lat: 49.08211028837632, lon: 19.28503290290511});
+  const refresher = React.useRef<any>();
 
-  useIonViewWillEnter(() => {
-    const msgs = getMessages();
-    setMessages(msgs);
-  });
+
+  React.useEffect(() => {
+    if(!loading) return;
+    fetch(`https://api.met.no/weatherapi/locationforecast/2.0/complete?lat=${coords.lat}&lon=${coords.lon}`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          if (refresher.current) refresher.current()
+          refresher.current = null
+          setLoading(false);
+          setData(result);
+        },
+        (error) => {
+          console.error(error);
+        }
+      )
+  }, [loading]);
+
+  const handleLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(({coords}) => {
+        setCoords({lat: coords.latitude, lon: coords.longitude})
+        setLoading(true)
+      })
+    }
+  }
 
   const refresh = (e: CustomEvent) => {
-    setTimeout(() => {
-      e.detail.complete();
-    }, 3000);
+    refresher.current = () => e.detail.complete()
+    setLoading(true);
   };
+
+  if (!data) return null;
+
+  const forecast = data.properties.timeseries[0];
 
   return (
     <IonPage id="home-page">
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Inbox</IonTitle>
+          <IonTitle>Predpoveď počasia</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonRefresher slot="fixed" onIonRefresh={refresh}>
           <IonRefresherContent></IonRefresherContent>
         </IonRefresher>
-
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">
-              Inbox
-            </IonTitle>
-          </IonToolbar>
-        </IonHeader>
-
-        <IonList>
-          {messages.map(m => <MessageListItem key={m.id} message={m} />)}
-        </IonList>
+        <IonFab vertical="top" horizontal="end" edge slot="fixed">
+          <IonFabButton onClick={handleLocation}>
+            <IonIcon icon={locate} />
+          </IonFabButton>
+        </IonFab>
+        <Forecast forecast={forecast} title="Aktuálne" />
+        <IonSlides pager={true}>
+          {data.properties.timeseries
+            .filter((forecast: any) => (new Date(forecast.time)).getHours() === 13)
+            .map((forecast: any) => (
+              <IonSlide key={forecast.time}>
+                <Forecast forecast={forecast} />
+              </IonSlide>
+            ))}
+        </IonSlides>
       </IonContent>
     </IonPage>
   );
